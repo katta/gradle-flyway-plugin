8016eb6e8fb7b9aa4c2879c1d751929fe0b5dee3
Srivatsa Katta <vatsa.katta@gmail.com>
2012-10-25 13:58:27 +0530

    Checking if init task creates schema_version and initializes

--- src/test/groovy/org/gradle/plugin/task/InitTaskTest.groovy
+++ src/test/groovy/org/gradle/plugin/task/InitTaskTest.groovy
@@ -1,12 +1,21 @@
 package org.gradle.plugin.task
 
-import org.junit.Test
 import org.gradle.plugin.FlywayPluginTestBase
+import org.junit.Test
 
 public class InitTaskTest extends FlywayPluginTestBase {
 
     @Test
     public void shouldExecuteInitTask() {
+        def sql = dbConnector()
+
+        def tablesResultSet = sql.connection.metaData.getTables(null, "*", "schema_version", null)
+        assert !tablesResultSet.next()
+
         findTask("flywayInit").execute();
+
+        def row = sql.firstRow("select * from schema_version")
+        assert "0".equals(row.getProperty("version"))
+        assert "INIT".equals(row.getProperty("type"))
     }
 }
