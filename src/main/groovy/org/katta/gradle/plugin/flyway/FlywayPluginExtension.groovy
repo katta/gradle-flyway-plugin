b2dd743b8c6e7def61595dfa7f41b25ae4ac5fa9
Srivatsa Katta <vatsa.katta@gmail.com>
2012-10-26 11:10:42 +0530

    added more flyway configurations to extension

--- src/main/groovy/org/katta/gradle/plugin/flyway/FlywayPluginExtension.groovy
+++ src/main/groovy/org/katta/gradle/plugin/flyway/FlywayPluginExtension.groovy
@@ -1,11 +1,20 @@
 package org.katta.gradle.plugin.flyway
 
 class FlywayPluginExtension {
-
     String driver;
     String url;
     String user;
     String password;
     String schemas;
-
+    String table;
+    String locations;
+    String sqlMigrationPrefix;
+    String sqlMigrationSuffix;
+    String encoding;
+    String placeholderPrefix;
+    String placeholderSuffix;
+    String target;
+    String validationMode;
+    String validationErrorMode;
+    String disableInitCheck;
 }
