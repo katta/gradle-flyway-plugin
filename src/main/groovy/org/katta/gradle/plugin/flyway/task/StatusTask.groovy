a21d18e22bd78deb63dd128112491b334eb0201c
Ralph van Etten <ralph@et10.org>
2013-07-11 13:49:38 +0200

    using flyway 2.1.1

--- src/main/groovy/org/katta/gradle/plugin/flyway/task/StatusTask.groovy
+++ src/main/groovy/org/katta/gradle/plugin/flyway/task/StatusTask.groovy
@@ -1,20 +1,20 @@
 package org.katta.gradle.plugin.flyway.task
 
 import com.googlecode.flyway.core.Flyway
-import com.googlecode.flyway.core.metadatatable.MetaDataTableRow
+import com.googlecode.flyway.core.api.MigrationInfo
 
 class StatusTask extends AbstractFlywayTask {
 
     @Override
     void executeTask(Flyway flyway) {
 
-        MetaDataTableRow metaTableRow = flyway.status()
+        MigrationInfo info = flyway.info().current()
         StringBuilder builder = new StringBuilder("-------------Schema Status------------\n")
-        builder.append("Version :").append(metaTableRow.getVersion()).append("\n")
-                .append("Description :").append(metaTableRow.getDescription()).append("\n")
-                .append("Type :").append(metaTableRow.getMigrationType()).append("\n")
-                .append("Installed On :").append(metaTableRow.getInstalledOn()).append("\n")
-                .append("State :").append(metaTableRow.getState())
+        builder.append("Version :").append(info.getVersion()).append("\n")
+                .append("Description :").append(info.getDescription()).append("\n")
+                .append("Type :").append(info.getType()).append("\n")
+                .append("Installed On :").append(info.getInstalledOn()).append("\n")
+                .append("State :").append(info.getState())
 
         println builder.toString()
     }
